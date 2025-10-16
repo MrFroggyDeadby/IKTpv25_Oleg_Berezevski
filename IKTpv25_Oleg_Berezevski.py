6db3edf196ec10d50e6206d0c004ab7d4b6ea1f5
opilane <opilane@HP-CZC2349HTQ>
2025-10-16 11:41:48 +0300

    hgdf

--- IKTpv25_Oleg_Berezevski.py
+++ IKTpv25_Oleg_Berezevski.py
@@ -103,3 +103,172 @@
 # sõbrad = int(input('Mitu sõbrad läksid Pitsa eest? '))
 # pitsa_hind = pitsa_hind/sõbrad
 # print(f'Iga sõber peab maksta {pitsa_hind:.2f}')
+
+#while True:
+#    paev_number=int(input('Sisesta päeva number(1-7): '))
+#    if paev_number==1:
+#        print('Esmaspäev')
+#    elif paev_number==2:
+#        print('Teisipäev')
+#    elif paev_number==3:
+#        print('Kolmapäev')
+#    elif paev_number==4:
+#        print('Neljapäev')
+#    elif paev_number==5:
+#        print('Reede')
+#    elif paev_number==6:
+#        print('Laupäev')
+#    elif paev_number==7:
+#        print('Pühapäev')
+#    else:
+#        print('Vale number!')
+
+
+
+
+#nimi = input('Mis on sinu nimi? ')
+
+
+#if nimi == 'JUKU':
+#    print('Lähme kinno')
+#    vanus = int(input('Kui vana sa oled?'))
+#    if vanus.isdigit:    
+#        if vanus < 6:
+#            print('Pilet on tasuta')
+#        elif vanus <= 14:
+#            print('lastepilet')
+#        elif vanus <= 65:
+#            print('täispilet')
+#        else:
+#            print('sooduspilet')
+#    else:
+#        print('Sisestage number!')
+#else:
+#    print('Kes sa oled???')
+
+#nimi_1 = input('Sisesta oma nimi: ').capitalize
+#nimi_2 = input('Sisesta oma nimi: ').capitalize
+
+#if nimi_1.isalpha and nimi_2.isalpha:
+#    if nimi_1 == 'Vitali' and nimi_2 == 'Artjom' or nimi_1 == 'Artjom' and nimi_2 == 'Vitali':
+#        print('On täna pinginaabrid')
+#    else:
+#        print('Pole täna pinginaabrid')
+#else:
+#    print('Sisestage tähed!!!')
+
+
+
+
+#pikkus = int(input('Sisestage pikkus: '))
+#laius = int(input('Sisestage laius: '))
+#if pikkus > 0 and laius > 0:
+ #   pindala = pikkus * laius
+  #  print(f'pindala suurus on {pindala}')
+  #  user = input('Kas soovite remondi teha? ').capitalize()
+  #  if user.isalpha() and user == 'Jah':
+    #    hind = float(input('Ruutmeetri hind? '))
+  #      if hind > 0:
+   #         remondi_hind = hind * pindala
+    #        print(f'Remondi summa on {remondi_hind}€')
+    #        kes = input('Kes teeb remondi(ise/töötaja)? ').capitalize()
+    #        if kes.isalpha() and kes == 'Ise':
+   #            print(f'Siis summa on {remondi_hind}€')
+  #  #        else:
+   #             print(f'Siis summa on {remondi_hind + 300}€')
+  #      else:
+    #        print('Hind ei saa olla negatiivne')
+ #   else:
+   #     print('Head aega!')
+
+#alghind = (input('Sisestage hind: '))
+
+#if alghind.isdigit():
+ #   alghind = float(alghind)
+#    if alghind >= 700:
+    #    hind = alghind * 0.7
+   #     print(f'Hind on {hind}')
+  #  else:
+ #       print(f'Hind on {alghind}')
+
+# Küsi temperatuur ning teata, kas see on üle 18 kraadi (soovitav toasoojus talvel)
+
+
+# try:
+#     temperatuur = float(input())
+#     if temp.isdigit():
+#         if temperatuur > 18:
+#             print('Soovitatav toasoojus talvel')
+#         else:
+#             print('Võib olla jahe')
+# except:
+#         print('Sisesta number!')
+
+
+# Küsi inimese pikkus ning teata, kas ta on lühike, keskmine või pikk (piirid pane ise paika)
+# Küsi inimeselt pikkus ja sugu ning teata, kas ta on lühike, keskmine või pikk (mitu tingimusplokki võib olla üksteise sees).
+
+# try:
+#     pikkus = float(input('Sisesta pikkus cm: '))
+#     sugu = input('Sisesta sugu (mees/naine): ').lower()
+#     if sugu == 'mees':
+#         if pikkus <= 170 and pikkus > 0:
+#             print('Oled lühike')
+#         elif pikkus <= 190 and pikkus > 0:
+#             print('Oled keskmine') 
+#         elif pikkus > 190 and pikkus > 0:
+#             print('Oled pikk')
+#     elif sugu == 'naine':
+#         if pikkus <= 160 and pikkus > 0:
+#             print('Oled lühike')
+#         elif pikkus <= 180 and pikkus > 0:
+#             print('Oled keskmine') 
+#         elif pikkus > 180 and pikkus > 0:
+#             print('Oled pikk')
+#     else:
+#         print('Sisesta mees või naine!')
+# except:
+#     print('andmed on valed')
+
+
+
+#Küsi inimeselt poes eraldi kas ta soovib osta piima, saia, leiba jne. Loo juhuslikud hinnad ja küsi mitu tükki tahad osta, kui tahad.
+#Teata, mis summa maksma läheb(Kuva ekraanil tšekk).
+
+from random import randint
+
+piim = randint(1, 2)
+sai = randint(2, 3)
+leib = randint(1, 4)
+
+
+
+try:
+    piim_inimene = input('Kas soovite osta piima? (jah/ei): ').lower()
+    if piim_inimene == 'jah':
+        piim_kogus = int(input('Mitu piima soovite osta? '))
+        piim_summa = piim * piim_kogus
+    else:
+        piim_kogus = 0
+        piim_summa = 0
+    sai_inimene = input('Kas soovite osta saia? (jah/ei): ').lower()
+    if sai_inimene == 'jah':
+        sai_kogus = int(input('Mitu saia soovite osta? '))
+        sai_summa = sai * sai_kogus
+    else:
+        sai_kogus = 0
+        sai_summa = 0
+    leib_inimene = input('Kas soovite osta leiba? (jah/ei): ').lower()
+    if leib_inimene == 'jah':
+        leib_kogus = int(input('Mitu leiba soovite osta? '))
+        leib_summa = leib * leib_kogus
+    else:
+        leib_kogus = 0
+        leib_summa = 0
+    tsekk = [piim_summa, sai_summa, leib_summa]
+    kokku = sum(tsekk)
+    print(f'Tšekk:\n Piim maksab {piim} * {piim_kogus}€\n Kokku {piim}€,\n sai maksab {sai} * {sai_kogus}\n Kokku {sai}€,\n leib maksab {leib} * {leib_kogus}\n Kokku {leib}€.\n'
+          f'Kokku tuleb maksta {kokku}€.\n'
+          f'Tänan, head aega!')
+except:
+    pass
