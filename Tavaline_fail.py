7de9cd0d75fcc0138ebb021d0bc68ea515e424e0
opilane <opilane@HP-CZC2349HTF>
2025-12-04 11:41:35 +0200

    Ülesanded funktsiooniga

---
+++ Tavaline_fail.py
@@ -0,0 +1,28 @@
+﻿from funktsioonid import *
+#is_year_kontroll funktsiooni kasutmaine
+# for i in range(3):
+#     aasta = int_kontrol(input("Sisesta aasta: "))
+#     if is_year_leap(aasta):
+#         print(f"{aasta} on liigaasta")
+#     else:
+#         print(f"{aasta} on tavaline aasta")
+
+
+
+
+#Arithmetic finktsiooni kasutamine
+# print("Lahendame/testime 5 arvutehet!")
+# for i in range(5):
+#     arv1 = float_kontrol(input("Sisesta esimene arv: "))
+#     arv2 = float_kontrol(input("Sisesta teine arv: "))
+
+#     t = input("Sisesta tehe (+, -, *, /): ")
+#     tulemus = arithmetic(arv1, arv2, t)
+#     print(f"{arv1} {t} {arv2} = {tulemus}")
+
+#square funktsiooni kasutamine
+
+for i in range(5):
+    arv1 = float_kontrol(input("Sisesta külje pikkus: "))
+    ümbermõõt, pindala, diagonaal = square(arv1)
+    print(f"Külje pikkus on {arv1}.\nümbermõõt on {ümbermõõt}.\npindala on {pindala}.\ndiagonaal on {diagonaal}")
---
+++ funktsioonid.py
@@ -0,0 +1,64 @@
+﻿def int_kontrol(sisend:str):
+    """Kontrollib, kas sisestatud andmed on teisendatavad int aruks
+    :param str sisend: kasutaja sisestatud andmed
+    :return/rtype: teisendatud int arv
+    """
+    while True:
+        try:
+            arv = int(sisend)
+            return arv
+        except ValueError:
+            sisend = input("Palun sisesta arv: ")
+
+def float_kontrol(sisend:str):
+    """Kontrollib, kas sisestatud andmed on teisendatavad float aruks
+    :param str sisend: kasutaja sisestatud andmed
+    :return/rtype: teisendatud float arv
+    """
+    while True:
+        try:
+            arv = float(sisend)
+            return arv
+        except ValueError:
+            sisend = input("Palun sisesta arv: ")
+
+def arithmetic(a:float, b:float, tehe:str)->any:
+    """Lihtne kalkulaator:
+    + — liitmine
+    - — lahutamine
+    * — korrutamine
+    / — jagamine
+    Muul juhul tagastab "Tundmatu tehe"
+    :param float a: esimene arv
+    :param float b: teine arv
+    :param str tehe: Tehe, mis tuleb teha
+    :return/rtype: tehte tulemus float või str
+    """
+    if tehe in ["+", "-", "*", "/"]:
+        if tehe == "/" and b==0:
+            vastus = "Nulliga jagamine pole lubatud"
+        else:
+            vastus = eval(f"{a}{tehe}{b}")
+    else:
+        vastus = "Tundmatu tehe"
+    return vastus
+
+
+def is_year_leap(aasta:int)->bool:
+    """Kontrollib, kas aasta on liigaasta
+    :param: int aasta: aasta arvuna
+    :return/rtype: True kui liigaasta, False kui tavaline aasta
+    """
+    if (aasta % 4 == 0 and aasta % 100 != 0):
+        return True
+    else:
+        return False
+
+def square(külg:float)->any:
+    """Arvutab ruudud ümbermõõdu, pindala ja diagonaali
+    :param float külg: ruudu külje pikkus
+    :return/rtype: ümbvermõõt, pindala, diagonaal"""
+    ümbermõõt = 4 * külg
+    pindala = külg ** 2 # või külg * külg
+    diagonaal = külg ** 0.5 * 2
+    return ümbermõõt, pindala, diagonaal
